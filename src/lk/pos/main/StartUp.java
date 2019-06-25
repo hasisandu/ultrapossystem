0c3745fe4c27b713041bc2a08856fab3a1c92093
hasisandu <hasikasandaruwan.mgtuk@gmail.com>
2019-06-26 00:05:02 +0530

    Initial commit

---
+++ src/lk/pos/main/StartUp.java
@@ -0,0 +1,28 @@
+package lk.pos.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class StartUp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root =FXMLLoader.load(this.getClass().getResource("/lk/pos/view/MainView.fxml"));
+        Scene mainScene= new Scene(root);
+        primaryStage.setTitle("Ultra Version 1.0");
+        primaryStage.setScene(mainScene);
+        primaryStage.setResizable(true);
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+    }
+}
